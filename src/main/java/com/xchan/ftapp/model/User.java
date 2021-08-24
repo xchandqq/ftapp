2a8f03437f3cddadcebddb203307c7f8061ddb2c
Christian Abellanosa <abellanosachristian@gmail.com>
2021-08-24 11:06:02 +0800

    User Model

---
+++ src/main/java/com/xchan/ftapp/model/User.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.xchan.ftapp.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Christian
+ */
+@Table
+@Entity
+public class User implements Serializable {
+    
+    @Id
+    private long Id;
+    
+    @Column
+    private String firstname;
+    
+    @Column
+    private String lastname;
+    
+    @Column
+    private String username;
+    
+    @Column
+    @JsonIgnore
+    private String password;
+    
+    @Column
+    private boolean activated;
+    
+    @Column
+    private String userRole;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long Id) {
+        this.Id = Id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActivated() {
+        return activated;
+    }
+
+    public void setActivated(boolean activated) {
+        this.activated = activated;
+    }
+
+    public String getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(String userRole) {
+        this.userRole = userRole;
+    }
+    
+    
+}
